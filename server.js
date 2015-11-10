df34514e86a37c73dd1826288bd1d293b665b69b
Wenson Tsai <wensontsai@gmail.com>
2015-11-10 12:46:26 -0500

    api routes for auth working

--- server.js
+++ server.js
@@ -33,12 +33,76 @@ app.use(morgan('dev'));
 app.get('/', function(req, res){
 	res.send('Hello!  The API is at http://localhost:' +port+ '/api');
 });
+app.get('/setup', function(req,res){
+
+	// ::::::::: FOR TESTING ::::::::::::::::
+		// create a sample user
+		var nick = new User({
+			name: 'person1',
+			password: 'password',
+			admin: true
+		});
+	// ::::::::: FOR TESTING ::::::::::::::::
+
+
+	nick.save(function(err){
+		if(err) throw err;
+
+		console.log('User saved successfully');
+		res.json( {success: true} );
+	});
+});
+
+
 
 // =================
 // API Routes
 // =================
+// get router instance for api routes
+var apiRoutes = express.Router();
 
+// route to auth user (POST http://localhost:8080/api/authenticate)
+apiRoutes.post('/authenticate', function(req, res){
+	// find user
+	User.findOne({name: req.body.name}, function(err, user){
+		if (err) throw err;
 
+		if (!user) {
+			res.json( {success: false, message: 'Authentication failed.  User not found!'} );
+			return;
+		} 
+		if(user.password !== req.body.password){
+			res.json( {success: false, message: 'Authentication failed.  Wrong password!'} );
+			return;
+		}
+
+		var token = jwt.sign(user, app.get('secret'), {
+			expiresInMinutes: 1440 //expires in 24hrs
+		});
+
+		res.json({
+			success: true,
+			message: 'Enjoy your token!',
+			token: token
+		});
+	});
+});
+
+// route for middleware to verify token
+
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function(req, res){
+	res.json( {message: 'Welcome to JWT HAWT API'});
+});
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function(req, res){
+	User.find({}, function(err, users){
+		res.json(users);
+	});
+});
+
+// apply the routes to our application with the prefix '/api'
+app.use('/api', apiRoutes);
 
 
 
